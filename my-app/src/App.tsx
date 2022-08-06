8b3bb6fc1e7a0fc8b9243b6a8e4680bac9c22fa4
arnavkasbi <53343740+arnavkasbi@users.noreply.github.com>
2022-08-06 13:11:50 +0400

    import routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Provider } from 'react-redux'
 import { store } from './stores'
+import { AppRoutes } from './layout/Routes';
 
 function App() {
   return (
     <Provider store={store}>
-          
+          <AppRoutes />
       </Provider>
   );
 }
