ac2e588f73070a6c43cf714c7a6ba9b69ddf95c6
arnavkasbi <53343740+arnavkasbi@users.noreply.github.com>
2022-08-06 13:10:06 +0400

    Added folder structure

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
 import { store } from './stores'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/login';
 
 function App() {
   return (
     <Provider store={store}>
-          <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        </Routes>
-        </BrowserRouter>
+          
       </Provider>
   );
 }
---
+++ my-app/src/layout/Routes.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import Login from '../pages/login';
+import { LOGIN_ROUTE } from "./appRoutes";
+
+
+
+type routesProps = Record<string, never>;
+
+export const AppRoutes: React.FC<routesProps> = () => {
+  return (
+    <BrowserRouter> <Routes>
+        {/* Default Route */}
+        <Route path="/" element={<Login />} />
+
+        {/* Page Routes */}
+        <Route path={LOGIN_ROUTE} element={<Login />} />
+        
+
+        {/* Faulty Route */}
+        <Route path="*" element={<Navigate to={LOGIN_ROUTE} replace />} />
+      </Routes></BrowserRouter>
+  );
+};
